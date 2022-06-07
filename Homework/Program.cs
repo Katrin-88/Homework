8cafd497a32b070a91e1b673c34b5ba29500b466
Ekaterina <kat_izh@mail.ru>
2022-06-07 12:27:13 +0300

    Task 1

---
+++ Homework/Program.cs
@@ -0,0 +1,16 @@
+﻿int num1, num2;
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 > num2)
+{
+    Console.Write("First number is max, second number is min");
+}
+else
+{
+    Console.Write("Second number is max, first number is min");
+}
