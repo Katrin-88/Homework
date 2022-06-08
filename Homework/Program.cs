f3e12793966eb442e7c01775805010fbf359c3a8
Ekaterina <kat_izh@mail.ru>
2022-06-08 17:12:23 +0300

    Homework Seminar 1

--- Homework/Program.cs
+++ Homework/Program.cs
@@ -1,4 +1,7 @@
-﻿int num1, num2;
+﻿/*
+//Задача1
+
+int num1, num2;
 
 Console.Write("Input first integer number: ");
 num1 = Convert.ToInt32(Console.ReadLine());
@@ -14,3 +17,61 @@ else
 {
     Console.Write("Second number is max, first number is min");
 }
+
+
+//Задача2
+
+int num1, num2, num3;
+
+Console.Write("Input first integer number: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second integer number: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input third integer number: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2 > max) max = num2;
+if(num3 > max) max = num3;
+
+Console.Write("Max number is: " + max);
+
+
+
+//Задача 3
+
+int num;
+
+Console.Write("Input your integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+if(num % 2 == 0)
+{
+    Console.Write("It's an even number");
+}
+else
+{
+    Console.Write("It an uneven number");
+}
+
+
+//Задача 4
+
+int num;
+
+Console.Write("Input your integer number: ");
+num = Convert.ToInt32(Console.ReadLine());
+
+int current = 2;
+
+while(current <= num)
+{
+    if(num % 2 == 0);
+    {
+    Console.Write(current + " ");
+    current = current + 2;}
+}
+*/
