5d2a35015ca6d774dfb7ed9477c5c4419f78567b
Ekaterina <kat_izh@mail.ru>
2022-06-19 18:43:46 +0300

    HomeWork Seminar4

---
+++ Homework4/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задача 1. Программа принимает 2 числа. Нужно число А взвести в степень B
+
+/*
+int Stepen(int numA, int numB)
+{
+  int result = 1;
+  for(int i = 1; i <= numB; i++)
+  {
+    result = result * numA;
+  }
+   return result;
+}
+
+  Console.Write("Input number A: ");
+  int numA = Convert.ToInt32(Console.ReadLine());
+  Console.Write("Input number B: ");
+  int numB = Convert.ToInt32(Console.ReadLine());
+
+  int ResultAB = Stepen(numA, numB);
+  Console.WriteLine("Result is: " + ResultAB);
+*/
+
+//Задача 2. Создать массив из 8 элементов
+
+int size = 8;
+int[] array = new int [size];
+for(int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(1,99);
+}
+for(int i = 0; i < size; i++)
+{
+    Console.Write(array[i] + " ");
+}
